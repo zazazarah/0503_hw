e98051bc30d91e3c9f8c9a58d58fe74449bcd0c9
zazazarah <mllezarah@gmail.com>
2016-05-05 18:37:11 -0700

    Added files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$(document).on("click", "#top-left", function() {
+		$(this)
+		.html("X");
+	});
+
+});
+
+//var turn = "X";
+
+//if (turn === "X") {
+	//click("X");
+//} else {
+//	click("O");
+//}
